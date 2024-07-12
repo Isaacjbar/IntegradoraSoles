d1eca0fd7b5988bb184f9deeda91159ee408a5c2
Isaac Jiménez <120062171+Isaacjbar@users.noreply.github.com>
2024-07-12 12:42:17 -0600

    Update historias-interactivas.sql

--- ScriptsDataBase/historias-interactivas.sql
+++ ScriptsDataBase/historias-interactivas.sql
@@ -1,57 +1,54 @@
--- Crear la base de datos si no existe y usarla
-CREATE DATABASE IF NOT EXISTS historiasinteractivas;
-USE historiasinteractivas;
+CREATE DATABASE IF NOT EXISTS historiaInteractiva;
+USE historiaInteractiva;
 
--- Creación de la tabla Usuarios
-CREATE TABLE IF NOT EXISTS Usuarios (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(100) NOT NULL,
-    Apellido VARCHAR(100) NOT NULL,
-    CorreoElectronico VARCHAR(100) UNIQUE NOT NULL,
-    Contraseña VARCHAR(100), 
+CREATE TABLE IF NOT EXISTS usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    correo_electronico VARCHAR(100) UNIQUE NOT NULL,
+    contrasena VARCHAR(255) NOT NULL,
     estado BOOLEAN NOT NULL,
     codigo VARCHAR(100),
-    -- Administrador = admin, Cliente = client
-    FechaRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Creación de la tabla Historias
-CREATE TABLE IF NOT EXISTS Historias (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    Titulo VARCHAR(255) NOT NULL,
-    AutorID INT NOT NULL,
-    FOREIGN KEY (AutorID) REFERENCES Usuarios(ID)
+CREATE TABLE IF NOT EXISTS historia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    autor_id INT NOT NULL,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (autor_id) REFERENCES usuario(id)
 );
 
--- Creación de la tabla Escenas
-CREATE TABLE IF NOT EXISTS Escenas ( 
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    HistoriaID INT NOT NULL,
-    Titulo VARCHAR(20) NOT NULL,
-    Video VARCHAR(1000),
-    Audio BLOB,   
-    Imagen BLOB,
-    Descripcion VARCHAR(1000),
-    EsFinal BOOLEAN DEFAULT FALSE,
-    TextoFinal VARCHAR(1000),
-    FOREIGN KEY (HistoriaID) REFERENCES Historias(ID)
+CREATE TABLE IF NOT EXISTS escena (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    historia_id INT NOT NULL,
+    titulo VARCHAR(40) NOT NULL,
+    video VARCHAR(1000),
+    audio VARCHAR(1000),
+    imagen VARCHAR(1000),
+    descripcion VARCHAR(1000),
+    es_final BOOLEAN DEFAULT FALSE,
+    texto_final VARCHAR(1000),
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (historia_id) REFERENCES historia(id)
 );
 
--- Creación de la tabla Decisiones
-CREATE TABLE IF NOT EXISTS Decisiones (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    EscenaID INT NOT NULL,
-    Descripcion VARCHAR(1000),
-    EscenaDestinoID INT,
-    FOREIGN KEY (EscenaID) REFERENCES Escenas(ID),
-    FOREIGN KEY (EscenaDestinoID) REFERENCES Escenas(ID)
+CREATE TABLE IF NOT EXISTS decision (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    escena_id INT NOT NULL,
+    descripcion VARCHAR(1000),
+    escena_destino_id INT,
+    fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (escena_id) REFERENCES escena(id),
+    FOREIGN KEY (escena_destino_id) REFERENCES escena(id)
 );
 
--- Creación de la tabla EstadosPublicacion
-CREATE TABLE IF NOT EXISTS EstadosPublicacion (
-    ID INT AUTO_INCREMENT PRIMARY KEY,
-    HistoriaID INT NOT NULL,
-    Estado VARCHAR(20) NOT NULL, -- 'Publicado', 'Oculto', etc.
-    FechaCambio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (HistoriaID) REFERENCES Historias(ID)
+CREATE TABLE IF NOT EXISTS estado_publicacion (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    historia_id INT NOT NULL,
+    estado VARCHAR(20) NOT NULL,
+    fecha_cambio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (historia_id) REFERENCES historia(id)
 );
