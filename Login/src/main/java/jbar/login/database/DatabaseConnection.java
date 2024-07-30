22f9c81bf186b983a155510f23ed37918bae4e12
ErickGS <20233tn149@utez.edu.mx>
2024-07-29 18:34:04 -0600

    Algunas correcciones del front

--- Login/src/main/java/jbar/login/database/DatabaseConnection.java
+++ Login/src/main/java/jbar/login/database/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/historiainteractiva";
     private static final String USER = "root";
-    private static final String PASSWORD = "erito28";
+    private static final String PASSWORD = "root";
 
     public static Connection getConnection() throws SQLException {
         try {
