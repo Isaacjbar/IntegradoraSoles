18ba49865bb9c255be39ef5632d76e86818bb82f
IsaacJimenez <20233tn182@utez.edu,mx>
2024-08-13 20:58:22 -0600

    FORMULARIOS

--- Login/src/main/java/jbar/login/database/DatabaseConnection.java
+++ Login/src/main/java/jbar/login/database/DatabaseConnection.java
@@ -18,3 +18,30 @@ public class DatabaseConnection {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+/*
+*
+* package jbar.login.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    // URL de conexión correcta
+    private static final String URL = "jdbc:mysql://db-historiainteractiva.chutbwctkgaj.us-east-1.rds.amazonaws.com:3306/historiaInteractiva";
+    private static final String USER = "IsaacJbar";
+    private static final String PASSWORD = "deol_bcl#18";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
+
+*
+*
+* */
